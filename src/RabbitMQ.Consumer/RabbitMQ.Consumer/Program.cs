b11e33552d46f240e4001441b668ec57c4ee4a46
aliyildiz <aliyyldiz@gmail.com>
2024-12-29 22:21:35 +0300

    init

---
+++ src/RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new("your-amqp-uri");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
+
+consumer.Received += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray(); 
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+    await Task.Delay(200);
+    channel.BasicAck(deliveryTag: eventArgs.DeliveryTag, multiple: false);
+};
+    
+Console.Read();
---
+++ src/RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+factory.Uri = new("your-amqp-uri");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive:false);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(50);
+    byte[] message = Encoding.UTF8.GetBytes("hello world " + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", basicProperties: null, body: message);
+}
+
+Console.Read();
